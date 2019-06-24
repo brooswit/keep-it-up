70f03cd05ef70b1e202f37e0f009935bec3b32cc
John Winstead <brooswit+github@gmail.com>
2019-06-24 10:56:51 -0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,11 +1,6 @@
-cd $1
-while [ 1 ]
-do
-    git checkout .
-    git pull origin $2
-    yarn update
-    yarn build
-    yarn start
-    echo "Pres CTRL+C to stop..."
-    sleep 1
-done
+cd ../$1
+git checkout .
+git pull origin $2
+yarn update
+yarn build
+yarn start
