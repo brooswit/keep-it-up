d3631fd2b0fa4bd7fce3e45af196e9bd252e0ffb
John Winstead <brooswit+github@gmail.com>
2019-06-24 10:50:55 -0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -3,9 +3,9 @@ while [ 1 ]
 do
     git checkout .
     git pull origin $2
-    npm update
-    npm run build
-    npm run start
+    yarn update
+    yarn build
+    yarn start
     echo "Pres CTRL+C to stop..."
     sleep 1
 done
