477d328063229704ac71462d30e2757f05a65364
John Winstead <brooswit+github@gmail.com>
2019-06-24 10:57:31 -0700

    Update main.sh

--- main.sh
+++ main.sh
@@ -2,7 +2,7 @@ while [ 1 ]
 do
     git checkout .
     git pull origin $(git symbolic-ref --short HEAD)
-    bash run.sh $1 $2
+    bash run.sh $1
     echo "Pres CTRL+C to stop..."
     sleep 1
 done
